0ceafac60bb9875759c80e8c516b7e8f8b709940
geechrist <geechrist@gmail.com>
2015-03-08 03:02:41 +0800

    add git aliase

--- modules/holman-git/aliases.zsh
+++ modules/holman-git/aliases.zsh
@@ -6,15 +6,18 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
-alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gl='git pull --prune'
 alias gp='git push origin HEAD'
 alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
+alias gc='git commit -v'
+alias gca='git commit -av'
 alias gco='git checkout'
-alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias ga='git add'
+alias gm='git merge'
+alias gf='git fetch'
+alias gsa='git stash' #git stash all
+alias gsp='git stash -p' #git stash partial
+alias gso='git stash pop' #git stash pop
